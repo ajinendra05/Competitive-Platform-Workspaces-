48f9a87d25426c2b9161496907c3ca26f2bcadbe
Ajinendra Singh Rajpoot <ajinendra05@gmail.com>
2023-08-05 17:14:57 +0530

    pointer

---
+++ pointer.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int arr[10], a[20];
+    cout<< arr << endl;
+    auto it = &arr;
+    int num = 0;
+    cout<< it << endl;
+    // arr = a;
+    cout<< it << endl;
+    cout<< &it << endl;
+
+    return 0;
+}
